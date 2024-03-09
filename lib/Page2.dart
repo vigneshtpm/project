f4e9d1255f21ef4e11555e89176bf9ff01f21ef7
vigneshtpm <vigneshwarantpm2@gmail.com>
2024-03-09 12:14:59 +0530

    8 commit

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -1,45 +1,21 @@
-import 'package:flutter/material.dart' ;
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:numberpicker/numberpicker.dart';
 import 'usercommand.dart';
 import 'package:provider/provider.dart';
 import 'dark_mode_provider.dart';
 
 class Page2 extends StatefulWidget {
-  const Page2({super.key});
+  const Page2({Key? key}) : super(key: key);
 
   @override
   _Page2State createState() => _Page2State();
 }
 
 class _Page2State extends State<Page2> {
-  int selectedWorkTimeInMinutes = 0;
-  int selectedBreakTimeInMinutes = 0;
-
-  Future<void> _selectWorkTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay(hour: selectedWorkTimeInMinutes ~/ 60, minute: selectedWorkTimeInMinutes % 60),
-    );
-
-    if (picked != null) {
-      setState(() {
-        selectedWorkTimeInMinutes = picked.hour * 60 + picked.minute;
-      });
-    }
-  }
-
-  Future<void> _selectBreakTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay(hour: selectedBreakTimeInMinutes ~/ 60, minute: selectedBreakTimeInMinutes % 60),
-    );
-
-    if (picked != null) {
-      setState(() {
-        selectedBreakTimeInMinutes = picked.hour * 60 + picked.minute;
-      });
-    }
-  }
+  int selectedWorkHours = 0;
+  int selectedWorkMinutes = 0;
+  int selectedBreakMinutes = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -53,117 +29,177 @@ class _Page2State extends State<Page2> {
         ),
         centerTitle: true,
         backgroundColor: darkModeProvider.darkModeEnabled ? Colors.black : Colors.blueAccent,
-        toolbarHeight: 60.0,
+        toolbarHeight: 30.0,
       ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(vertical: 130.0, horizontal: 20.0),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 130.0, horizontal: 20.0),
         child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: InkWell(
-                onTap: () => _selectWorkTime(context),
-                child: InputDecorator(
-                  decoration: const InputDecoration(
-                    labelText: 'Select Working Time',
-                    labelStyle: TextStyle(color: Colors.white),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 2, color: Colors.white),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildTimePicker(
+                label: 'Select Working Time',
+                hoursValue: selectedWorkHours,
+                minutesValue: selectedWorkMinutes,
+                onHoursUpdate: (value) {
+                  setState(() {
+                    selectedWorkHours = value;
+                  });
+                },
+                onMinutesUpdate: (value) {
+                  setState(() {
+                    selectedWorkMinutes = value;
+                  });
+                },
+              ),
+              buildBreakTimePicker(
+                label: 'Select Break Time',
+                minutesValue: selectedBreakMinutes,
+                onMinutesUpdate: (value) {
+                  setState(() {
+                    selectedBreakMinutes = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  HapticFeedback.lightImpact();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => UserFunc(
+                        workTime: selectedWorkHours * 60 + selectedWorkMinutes,
+                        breakTime: selectedBreakMinutes,
                       ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 2, color: Colors.white38),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30.0),
-                      ),
-                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 34),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        "${selectedWorkTimeInMinutes ~/ 60} hours ${selectedWorkTimeInMinutes % 60} minutes",
-                        style: const TextStyle(fontSize: 17.0, color: Colors.white),
-                      ),
-                    ],
+                  elevation: 5,
+                ),
+                child: const Text(
+                  "Next",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: InkWell(
-                onTap: () => _selectBreakTime(context),
-                child: InputDecorator(
-                  decoration: const InputDecoration(
-                    labelText: 'Select Break Time',
-                    labelStyle: TextStyle(color: Colors.white),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 2, color: Colors.white),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30.0),
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 2, color: Colors.white38),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30.0),
-                      ),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        "${selectedBreakTimeInMinutes ~/ 60} hours ${selectedBreakTimeInMinutes % 60} minutes",
-                        style: const TextStyle(fontSize: 17.0, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                HapticFeedback.lightImpact();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => UserFunc(
-                      workTime: selectedWorkTimeInMinutes,
-                      breakTime: selectedBreakTimeInMinutes,
-                    ),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 34),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  //side: const BorderSide(color: Colors.white, width: 1),
-                ),
-                elevation: 5,
-              ),
-              child: const Text(
-                "Next",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-        ),
+    );
+  }
+
+  Widget buildTimePicker({
+    required String label,
+    required int hoursValue,
+    required int minutesValue,
+    required void Function(int) onHoursUpdate,
+    required void Function(int) onMinutesUpdate,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Text(
+            label,
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 2),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                NumberPicker(
+                  value: hoursValue,
+                  minValue: 0,
+                  maxValue: 11,
+                  onChanged: onHoursUpdate,
+                  itemWidth: 80,
+                  itemHeight: 50,
+                  textStyle: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'HH',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                const SizedBox(width: 20),
+                NumberPicker(
+                  value: minutesValue,
+                  minValue: 0,
+                  maxValue: 59,
+                  onChanged: onMinutesUpdate,
+                  itemWidth: 80,
+                  itemHeight: 50,
+                  textStyle: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'MM',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBreakTimePicker({
+    required String label,
+    required int minutesValue,
+    required void Function(int) onMinutesUpdate,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Text(
+            label,
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 2),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                NumberPicker(
+                  value: minutesValue,
+                  minValue: 0,
+                  maxValue: 59,
+                  onChanged: onMinutesUpdate,
+                  itemWidth: 80,
+                  itemHeight: 50,
+                  textStyle: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'MM',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
