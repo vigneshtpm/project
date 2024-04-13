08a988e7ac5fcd5a3da2dc6422de72b6866fab90
vigneshtpm <vigneshwarantpm2@gmail.com>
2024-04-13 19:14:24 +0530

    11 commit

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -6,20 +6,17 @@ import 'Notification_service.dart';
 import 'package:provider/provider.dart';
 import 'dark_mode_provider.dart';
 
-
-const int kWorkDurationInSeconds = 20*60;
+const int kWorkDurationInSeconds = 20 * 60;
 const int kRestDurationInSeconds = 20;
 
 class Page1 extends StatefulWidget {
-  const Page1({super.key});
+  const Page1({Key? key}) : super(key: key);
 
   @override
   _Page1State createState() => _Page1State();
-
 }
 
 class _Page1State extends State<Page1> {
-
   final NotificationService notificationService = NotificationService();
   double percent = 0.0;
   bool isRunning = false;
@@ -29,20 +26,78 @@ class _Page1State extends State<Page1> {
   bool isWorkPhase = false;
   bool isButtonDisabled = false;
   bool isForegroundTaskRunning = true;
-
+  bool isPaused = false;
 
   @override
-  void initState(){
+  void initState() {
     notificationService.initNotification();
     notificationService.requestNotificationPermission();
     isWorkPhase = elapsedTime <= kWorkDurationInSeconds;
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     final darkModeProvider = Provider.of<DarkModeProvider>(context);
+
+    ElevatedButton startButton = ElevatedButton(
+      onPressed: () {
+        if (isPaused) {
+          startTimer(); // Resume timer
+          setState(() {
+            isPaused = false;
+          });
+        } else {
+          startTimer();
+          setState(() {
+            isButtonDisabled = true;
+          });
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 34),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        elevation: 5,
+      ),
+      child: const Text(
+        "Start",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 17,
+        ),
+      ),
+    );
+
+    ElevatedButton pauseButton = ElevatedButton(
+      onPressed: () {
+        pauseTimer();
+        setState(() {
+          isPaused = true;
+          isButtonDisabled = false; // Enable the start button when paused
+        });
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orangeAccent, // Change color to yellow for pause button
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 34),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          //side: const BorderSide(color: Colors.white, width: 0),
+        ),
+        elevation: 8,
+        shadowColor: Colors.grey,
+      ),
+      child: const Text(
+        "Pause",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 17,
+        ),
+      ),
+    );
+
     return Scaffold(
       backgroundColor: darkModeProvider.darkModeEnabled ? Colors.black : Colors.blueAccent,
       appBar: AppBar(
@@ -54,104 +109,87 @@ class _Page1State extends State<Page1> {
         backgroundColor: darkModeProvider.darkModeEnabled ? Colors.black : Colors.blueAccent,
         toolbarHeight: 60.0,
       ),
-        body: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
-          padding: const EdgeInsets.symmetric(vertical: 130.0, horizontal: 20.0),
-          child: Center(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 130.0, horizontal: 20.0),
+        child: Center(
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              isWorkPhase ? 'Working Time' : 'Break Time',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: isWorkPhase ? 24.0 : 22.0,  // Adjust the font size for Working Time label
-                color: Colors.white,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                isWorkPhase ? 'Working Time' : 'Break Time',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: isWorkPhase ? 24.0 : 22.0,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            SizedBox(height: isWorkPhase ? 30.0 : 25.0),
-            CircularPercentIndicator(
-              radius: 138.0,
-              lineWidth: 15.0,
-              animation: true,
-              animateFromLastPercent: true,
-              percent: percent,
-              center: Text(
-                formatTime(
+              SizedBox(height: isWorkPhase ? 30.0 : 25.0),
+              CircularPercentIndicator(
+                radius: 138.0,
+                lineWidth: 15.0,
+                animation: true,
+                animateFromLastPercent: true,
+                percent: percent,
+                center: Text(
+                  formatTime(
                     elapsedTime < kWorkDurationInSeconds
                         ? kWorkDurationInSeconds - elapsedTime
-                        : kRestDurationInSeconds - (elapsedTime - kWorkDurationInSeconds)),
-                style: TextStyle(
+                        : kRestDurationInSeconds - (elapsedTime - kWorkDurationInSeconds),
+                  ),
+                  style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 60.0,
-                    color:darkModeProvider.darkModeEnabled ? Colors.white : Colors.white,
+                    color: darkModeProvider.darkModeEnabled ? Colors.white : Colors.white,
+                  ),
                 ),
+                footer: const Text(
+                  "",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+                ),
+                circularStrokeCap: CircularStrokeCap.round,
+                backgroundColor: Colors.white,
+                progressColor: darkModeProvider.darkModeEnabled ? Colors.black : Colors.blueAccent,
               ),
-              footer: const Text(
-                "",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  isRunning ? pauseButton : startButton,
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      stopTimer();
+                      setState(() {
+                        isButtonDisabled = false;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 34),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        //side: const BorderSide(color: Colors.white, width: 0),
+                      ),
+                      elevation: 5,
+                    ),
+                    child: const Text(
+                      "Stop",
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ),
+                ],
               ),
-              circularStrokeCap: CircularStrokeCap.round,
-              backgroundColor: Colors.white,
-              progressColor: darkModeProvider.darkModeEnabled ? Colors.black : Colors.blueAccent,
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: isButtonDisabled ? null : () {
-                    startTimer();
-                    setState(() {
-                      isButtonDisabled = true;
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 34),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      //side: const BorderSide(color: Colors.white, width: 0),
-                    ),
-                    elevation: 5,
-                  ),
-                  child: const Text(
-                    "Start",
-                    style: TextStyle(color: Colors.white,fontSize: 17,),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    stopTimer();
-                    setState(() {
-                      isButtonDisabled = false;
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 34),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      //side: const BorderSide(color: Colors.white, width: 0),
-                    ),
-                    elevation: 5,
-                  ),
-                  child: const Text(
-                    "Stop",
-                    style: TextStyle(color: Colors.white,fontSize: 17,),
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-        ),
     );
   }
+
   void showNotification(String title, String body) {
     notificationService.showNotification(title: title, body: body);
   }
+
   void startTimer() {
     if (!isRunning) {
       timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
@@ -160,11 +198,10 @@ class _Page1State extends State<Page1> {
             // Work cycle
             percent = elapsedTime / kWorkDurationInSeconds;
             if (elapsedTime == 1) {
-              showNotification(" Start Work","The work is Started");
+              showNotification(" Start Work", "The work is Started");
               playSound();
-            }
-            else if (elapsedTime == kWorkDurationInSeconds) {
-              showNotification(" Stop Work","The work is Started");
+            } else if (elapsedTime == kWorkDurationInSeconds) {
+              showNotification(" Stop Work", "The work is Started");
               playSound();
             }
             // Add any additional conditions or actions as needed for the work cycle
@@ -177,7 +214,6 @@ class _Page1State extends State<Page1> {
             resetTimer();
             startTimer();
           }
-
           elapsedTime++;
           isWorkPhase = elapsedTime <= kWorkDurationInSeconds;
         });
@@ -188,7 +224,7 @@ class _Page1State extends State<Page1> {
 
   void resetTimer() {
     timer?.cancel();
-    isRunning=false;
+    isRunning = false;
     setState(() {
       elapsedTime = 0;
       percent = 0.0;
@@ -206,6 +242,12 @@ class _Page1State extends State<Page1> {
     }
   }
 
+  void pauseTimer() {
+    if (isRunning) {
+      timer?.cancel();
+      isRunning = false;
+    }
+  }
 
   void playSound() async {
     await audioPlayer.setAsset('assets/audio/sample1.mp3');
